5f962333c11fd845ced073da691c3a16a80307c7
Tyler Jewell <tjewell@codenvy.com>
2014-11-06 09:51:14 -0800

    Change site

--- index.php
+++ index.php
@@ -1,1 +1,104 @@
-<?php header('Location: http://docs.codenvy.com/che/'); ?>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Eclipse Che</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="http://www.eclipse.org/flux/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="http://www.eclipse.org/flux/css/flux-web.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="container">
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Eclipse Che</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="https://github.com/codenvy/sdk">Source code</a></li>
+            <li><a href="http://docs.codenvy.com/che">Docs</a></li>
+          </ul>
+		  <div class="navbar-nav navbar-form">
+			  <a class="btn btn-sm btn-warning" href="https://bugs.eclipse.org/bugs/enter_bug.cgi?product=Che" role="button">Report a bug</a>
+		  </div>
+		  <div class="navbar-nav navbar-form">
+			  <a class="btn btn-sm btn-info" href="https://dev.eclipse.org/mailman/listinfo/che-dev" role="button">Join the discussion</a>
+		  </div>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="http://www.eclipse.org">Eclipse.org</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+	  
+<a href="https://github.com/codenvy/sdk"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"></a>
+
+    </div>
+
+      <!-- Jumbotron -->
+      <div class="jumbotron">
+		  
+        <p style="font-size:18px">Eclipse Che is a cloud IDE and a platform for creating cloud IDE extensions. Che ships with 55 extensions for Java, JavaScript, AngularJS, git, and Docker. Package your own Che extensions to create beautiful, cross-browser developer tooling.</p>
+        
+      </div>
+
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-lg-4">
+          <h2>Code on the Web</h2>
+          <p>Use your browser to program anywhere on any machine in any programming language. Che is an IDE that can run within Servlet containers and accessible with most browsers.  You can run Che on your desktop, or try the hosted version at Codenvy.com.</p>
+        </div>
+        <div class="col-lg-4">
+          <h2>Real Language Tooling</h2>
+          <p>With dozens of pre-packaged extensions, you can create applications that use interpreted and compiled programming languages.  Extensions exist for Java, CodeMirror, XML, JavaScript, maven, ant, git, subversion, and many more.</p>
+       </div>
+        <div class="col-lg-4">
+          <h2>Create Own Extensions</h2>
+          <p>Write cloud tooling extensions using Java, GWT, and JavaScript. Program extensions using Che or the Eclipse IDE. Plenty of tutorials provided and an active community of 40 engineers developing new extensions every day.</p>
+        </div>
+	  </div>
+
+	  <hr/>
+	  
+      <div class="row">
+        <div class="col-lg-6">
+          <h2>Getting started</h2>
+          <p>You can build Che from <a href="https://github.com/codenvy/sdk" target="_blank" >source</a> or <a href="http://docs.codenvy.com/download/#eclipse-che-binaries" target="_blank" >download the Che binaries</a>.</p>
+          <p><a class="btn btn-primary" href="https://codenvy.com/f?id=3un9iy22c5jum571" role="button" target="_blank">Or try hosted Che at codenvy.com</a></p>
+        </div>
+        <div class="col-lg-6">
+          <h2>Join the project</h2>
+          <p>Interested in working with us on Che?  Interested in bringing your own language tooling to Che? The micro-service architecture of Che makes it easy to collaborate.  Join us on GitHub and on our mailing list.</p>
+          <p><a class="btn btn-primary" href="https://dev.eclipse.org/mailman/listinfo/che-dev" role="button" >The Che developer mailing list &raquo;</a></p>
+       </div>
+	  </div>
+
+      <!-- Site footer -->
+      <div class="footer">
+        <p>Copyright &copy; 2014 The Eclipse Foundation. All Rights Reserved. - <a href="http://www.eclipse.org/legal/privacy.php">Privacy Policy</a> - <a href="http://www.eclipse.org/legal/termsofuse.php">Terms of Use</a> - <a href="http://www.eclipse.org/legal/copyright.php">Copyright Agent</a> - <a href="http://www.eclipse.org/legal/">Legal</a> - <a href="http://www.eclipse.org">Eclipse.org</a></p>
+      </div>
+
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="http://www.eclipse.org/flux/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
