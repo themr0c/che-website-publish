65502649e292e9e56516627af48524e405154145
Tyler Jewell <tjewell@codenvy.com>
2016-06-25 12:49:00 -0700

    updated extend page

--- extend/index.php
+++ extend/index.php
@@ -49,11 +49,72 @@
     <div class="col-sm-6">
       <img src="../images/logo-extend-codenvy@2x.jpg" alt="Codenvy">
       <h2>Codenvy</h2>
-      <p>As the original donors of Eclipse Che, Codenvy continues to support the project while building  on top of Che a set of enterprise-focused features demanded are by IT: multi-tenancy; elastic scalability; LDAP integration; security and resource controls.</p>
+      <p>Codenvy provides a multi-user, multi-tenant and elastic impelementation of Eclipse Che with enterprise features including LDAP, single sign-on, security, and resource controls.</p>
       <a href="https://codenvy.com/product/technology/" class="btn-yellow" target="_blank">Learn More</a>  
     </div>
   </div> <!-- /.row -->
 </div> <!-- /.container-fluid -->
+
+<div class="tech">
+  <div class="container-fluid content">
+    <h2>Plug-Ins</h2>
+    <p>Che includes a growing set of <a href="https://eclipse-che.readme.io/docs/plug-ins">plug-ins</a>. You can also <a href="https://eclipse-che.readme.io/docs/developing-extensions">create and package</a> your own. You can also extend Che's runtimes, stacks, commands, look-and-feel. We are starting to build out customization <a href="https://github.com/eclipse/che/blob/master/CUSTOMIZING.md">resource center</a>.</p>
+    <div class="row">
+      <div class="col-md-3 col-sm-6">
+        <h5>Languages</h5>
+        <p>Syntax highlighting, stacks, templates, and code assistants.</p>
+        <ul>
+          <li>Java</li>
+          <li>C++</li>
+          <li>JavaSript</li>
+          <li>Python</li>
+          <li>PHP</li>
+          <li>Ruby</li>
+          <li>SQL</li>
+        </ul>
+      </div>
+
+      <div class="col-md-3 col-sm-6">
+        <h5>Frameworks</h5>
+        <p>Project types, scaffold tool integration, and syntax analysis.</p>
+        <ul>
+          <li><a href="https://www.eclipse.org/che/artik/">Samsung ARTIK</a></li>
+          <li><a href="https://www.eclipse.org/che/sap/">SAP HANA</a></li>
+          <li><a href="https://eclipse-che.readme.io/docs/openshift-config" target="_blank">Red Hat OpenShift</a></li>
+          <li>Angular JS</li>
+          <li>Docker</li>
+          <li>Yeoman</li>
+        </ul>
+      </div>
+
+      <div class="clearfix"></div>
+
+      <div class="col-md-3 col-sm-6">
+        <h5>Builders</h5>
+        <p>Builders to manage unit tests, compilation, linking, and dependencies.</p>
+        <ul>
+          <li>Ant</li>
+          <li>Bower</li>
+          <li>Grunt</li>
+          <li>Gulp</li>
+          <li>Maven</li>
+          <li>Npm</li>
+        </ul>
+      </div>
+
+      <div class="col-md-3 col-sm-6">
+        <h5>Tools</h5>
+        <p>Extensions that integrate the IDE with other parts of the developer tool chain.</p>
+        <ul>
+          <li>Git</li>
+          <li>Orion</li>
+          <li>SSH</li>
+          <li>Subversion</li>
+        </ul>
+      </div>
+    </div> <!-- .row -->
+  </div> <!-- .container-fluid -->
+</div> <!-- .tech -->
 
 
 <div class="bg-gray5 get-help">
