3bd689fd320fe91ceea5d665cd06fc414af6c7d6
Tyler Jewell <tjewell@codenvy.com>
2016-06-26 08:04:25 -0700

    download page

--- download/index.php
+++ download/index.php
@@ -15,8 +15,8 @@
   <div class="container-fluid">
     <h1>Downloads</h1>
     <h3>
-      Release: <span class="text-yellow">4.3.0</span><br>
-      Date: <span class="text-yellow">June 17, 2016</span>
+      Release: <span class="text-yellow">4.3.5</span><br>
+      Date: <span class="text-yellow">June 24, 2016</span>
     </h3>
   </div> <!-- /.container-fluid -->
 </div> <!-- /.jumbotron -->
@@ -26,22 +26,25 @@
 <div class="container-fluid content">    
   
 
-  <h2>Download Options</h2>
-  <p>The official signed Eclipse downloads are below, these are typically 1+ releases behind and require the correct Java and Docker versions.</p>
+  <h2>Installation Options</h2>    
+    <li>SaaS Cloud: <a href="<?php echo $rootPath; ?>/getting-started/cloud/">Hosted @ Codenvy.com</a></li></br>
+    
+    <li>Private Cloud: <a href="https://bitnami.com/stack/eclipse-che">Hosted @ AWS / EC2 / Azure by Bitnami</a></li>
 
-  <p>The latest version of Eclipse Che is available for download: </br>
-    <li><a href="<?php echo $rootPath; ?>/getting-started/download/">Vagrant</a></li></br>
-    <li><a href="<?php echo $rootPath; ?>/getting-started/download/">Docker</a></li></br>
-    <li><a href="https://marketplace.yatta.de/profiles/iQBd" target="_blank">Yatta Profile</a></li>
+    <li>Download with Vagrant: <a href="<?php echo $rootPath; ?>/getting-started/download/">Quick Install</a></li></br>
 
-<p>Eclipse Che is available as a SaaS cloud or private cloud:</br>
-<li><a href="<?php echo $rootPath; ?>/getting-started/cloud/">SaaS Cloud hosted at codenvy.com</a> </li></br>
-<li><a href="https://bitnami.com/stack/eclipse-che">Deployed to your private cloud by Bitnami</a></li>
+    <li>Download with Yatta: <a href="https://marketplace.yatta.de/profiles/iQBd">Automated Marketplace Install</a></li></br>
+
+    <li>Docker: <a href="https://eclipse-che.readme.io/docs/usage-docker#using-docker-syntax">Run as a Container</a></li></br>
+
+    <li>Download with Installers: <a href="#installers">JAR, EXE Extractors by Codenvy</a></li></br>
+
+    <li>Manual with Packages: <a href="#signed">Eclipse Signed ZIP/TGZ</a></li></br>
 
   <div class="separator"></div>
 
-  <h2>Eclipse Signed Packages</h2>
-  <p><a href="https://eclipse.org/che/getting-started">Packages let you run Che server, for the best performance. Packages extract Eclipse Che into a working directory. You must have </a><a href="https://www.java.com/en/download/">Java 1.8</a> installed. Windows and MacOS also require <a href="https://www.docker.com/docker-toolbox">Docker Toolbox</a> to be separately installed, which itself installs Git Bash, Virtual Box, and Docker.</p>
+  <h2><a id="signed">Eclipse Signed Packages</a></h2>
+  <p>Packages let you run Che natively. These packages extract Che into a directory. You must have </a><a href="https://www.java.com/en/download/">Java 1.8</a> and <a href="https://www.docker.com/">Docker</a> installed. Docker for Windows is currently supported. You can optionally use Virtual Box to launch Docker with Boot2Docker.</p>
 
   <ul>
     <li><h4>ZIP Package for Windows, Mac, and Linux</h4></li>
@@ -59,29 +62,27 @@
     <li><a href="https://install.codenvycorp.com/che/eclipse-che-nightly.tar.gz">❯ Download unsigned nightly from Codenvy servers (135 MB)</a></li>
   </ul>
 
-  <ul>
-    <li><h4>JAR Universal Extractor</h4></li>
-    <li class="monospace"><i>java -jar &lt;jar-file&gt;</i></li>
-    <li><a href="https://install.codenvycorp.com/che/eclipse-che-latest.jar">❯ Download unsigned latest release from Codenvy servers (138 MB)</a></li>
-    <li><a href="https://install.codenvycorp.com/che/eclipse-che-nightly.jar">❯ Download unsigned nightly from Codenvy servers (138 MB)</a></li>
-  </ul>
   <p>Need help getting Che to run? Please check the oodles of <a href="https://eclipse-che.readme.io/docs/">installation and usage docs</a>.</p>
   <div class="separator"></div>
 
-  <h2>Installers</h2>
-  <p>Installers install Eclipse Che and all its platform dependencies including Java, Git Bash, Docker Toolbox, and Virtual Box.</p>
+  <h2><a id="installers">Installers</a></h2>
+  <p>Installers install Eclipse Che and its platform dependencies including Java, Git Bash, Docker Toolbox, and Virtual Box. These installers are deprecated and will eventually be removed in favor of runnin Che as a Docker container. We will support these installers through the end of 2016.</p>
 
   <ul>
     <li><h4>EXE Installer for Windows 7+</h4></li>
     <li><i>Installs Che, Java, Docker, Git Bash, and Shortcuts</i></li>
     <li><a href="https://install.codenvycorp.com/che/eclipse-che-latest.exe">❯ Download unsigned latest release from Codenvy servers (416 MB)</a></li>
   </ul>
+
+  <ul>
+    <li><h4>JAR Universal Extractor</h4></li>
+    <li class="monospace"><i>java -jar &lt;jar-file&gt;</i></li>
+    <li><a href="https://install.codenvycorp.com/che/eclipse-che-latest.jar">❯ Download unsigned latest release from Codenvy servers (138 MB)</a></li>
+    <li><a href="https://install.codenvycorp.com/che/eclipse-che-nightly.jar">❯ Download unsigned nightly from Codenvy servers (138 MB)</a></li>
+  </ul>
+
   <div class="separator"></div>
 
-  <h2>Docker</h2>
-  <p>You can run the Che server as a Docker image. There are two ways to achieve this. First, you can <b><a href="https://eclipse-che.readme.io/docs/usage#command-line">install Che and then use the Che command line</a></b> to launch Che on localhost or in a Docker container created by Che. Second, you can <b><a href="https://eclipse-che.readme.io/docs/usage-docker">launch a Docker container containing Che</a></b> with any Docker client.</p>
-  <div class="separator"></div>
-  
   <h2>Raspberry Pi</h2>
   <p>The community has instructions for running Eclipse Che as an embedded IDE on a Raspberry Pi. Boot up, get an IP address, and start developing! Amazing work from Benjamin Cabe. <b><a href="https://blog.benjamin-cabe.com/2016/04/01/running-eclipse-che-on-a-raspberry-pi">Read the setup</a></b> instructions.</p>
   <div class="separator"></div>
