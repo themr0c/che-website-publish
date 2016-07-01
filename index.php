b3cf7e67eb04f708dfc1ec6a734eb7755d95a35b
Tyler Jewell <tjewell@codenvy.com>
2016-07-01 09:52:25 -0700

    button

--- index.php
+++ index.php
@@ -60,7 +60,7 @@
       <img src="images/logo-redhat@2x.png" alt="" class="img-responsive">
       <h3>Che at Red Hat</h3>
       <p>See Che used to do cloud native development in Paul Cormier's Red Hat Summit keynote.</p>
-      <a href="https://www.youtube.com/embed/wMTgIIJ-oqQ?start=2995&autoplay=1">Watch the Video</a>
+      <a href="https://www.youtube.com/embed/wMTgIIJ-oqQ?start=2995&autoplay=1" class="btn-yellow">Watch the Video</a>
     </div>
     <div class="col-md-4">
       <img src="images/img-home-whatsnew2@2x.png" alt="" class="img-responsive">
