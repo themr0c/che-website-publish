d29f10341d77e39dbc3cf49434dc0448445645a1
Stevan Le Meur <stevan.lemeur@gmail.com>
2016-08-15 11:20:38 +0200

    Update the homepage with 4.6 release

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
 
 <div class="annoucement">
   <div class="container-fluid">
-    <p>Read the Eclipse Che <a href="http://che.eclipse.org/release-notes-eclipse-che-4-5" target="_blank">4.5 Release Notes</a></p>
+    <p>Read the Eclipse Che <a href="http://che.eclipse.org/release-notes-eclipse-che-4-6/" target="_blank">4.6 Release Notes</a></p>
   </div>
 </div>
 
