b423e6a88afe3de908516ff67e9ce1d177313f52
Tyler Jewell <tjewell@codenvy.com>
2016-10-13 19:04:56 -0700

    checonf

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
 
 <div class="annoucement">
   <div class="container-fluid">
-    <h3><a href="https://medium.com/eclipse-che-blog/release-notes-eclipse-che-4-7-e8ba27a0c54d#.gi8zhpvk5" target="_blank">4.7 Released!</a> Major usability advances with tabbed terminals and multi-panel editors.</a></h3>
+    <h3><a href="http://eclipse.org/che/checonf" target="_blank">Register for CheConf 2016!</a> A virtual conference for cloud development.</a></h3>
   </div>
 </div>
 
