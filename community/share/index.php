407d21d664ce3f49df1c685e1bab36d75b4085a6
Stevan Le Meur <slemeur@redhat.com>
2018-05-18 20:13:16 +0200

    Remove Marketo forms

--- community/share/index.php
+++ community/share/index.php
@@ -23,12 +23,7 @@
     <div class="col-sm-6">
       <h3>Are you developing with Eclipse Che?</h3>
       <p>Tell us about your development project and Eclipse Che experience, and we will share it on social with our community and post your story on our blog.</p>
-    </div>
-
-    <div class="col-sm-6">
-      <script src="//app-ab05.marketo.com/js/forms2/js/forms2.min.js"></script>
-      <script>MktoForms2.loadForm("//app-ab05.marketo.com", "519-AYY-549", 1296);</script>
-      <form id="mktoForm_1296"></form>
+      <p>Reach us at che-dev@eclipse.org<p>
     </div>
   </div>
 </div> <!-- .container-fluid -->
