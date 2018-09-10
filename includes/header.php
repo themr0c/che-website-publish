79a5be5c97389ea7a706922633159fef9dbe6aae
Stevan Le Meur <slemeur@redhat.com>
2018-09-10 11:53:13 +0200

    fix github button
    Signed-off-by: Stevan Le Meur <slemeur@redhat.com>

--- includes/header.php
+++ includes/header.php
@@ -23,8 +23,8 @@
             <li><a target="_blank"  href="https://medium.com/eclipse-che-blog">Blog</a></li>
             <!--<li><a href="<?php echo $rootPath; ?>/media/">Articles</a></li>-->
             <li><a class="btn-yellow" href="<?php echo $rootPath; ?>/getting-started/">Get Started</a></li>
-            <!--<li><a class="github-button" href="https://github.com/eclipse/che" data-icon="octicon-star" data-show-count="true" aria-label="Star eclipse/che on GitHub">Star</a></li> -->
-            <!-- <li><iframe src="https://ghbtns.com/github-btn.html?user=eclipse&repo=che&type=star&count=true" frameborder="0" scrolling="0" width="100px" height="20px"></iframe></li> -->
+            <li><a class="github-button" href="https://github.com/eclipse/che" data-icon="octicon-star" data-show-count="true" aria-label="Star eclipse/che on GitHub" width="100px" height="20px">Star</a></li>
+            <!--<li><iframe src="https://ghbtns.com/github-btn.html?user=eclipse&repo=che&type=star&count=true" frameborder="0" scrolling="0" width="100px" height="20px"></iframe></li> -->
           </ul>
         </nav>
 
