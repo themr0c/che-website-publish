add51b325c21a0aa10b3bc315e01f672e52bdc2e
Stevan Le Meur <slemeur@redhat.com>
2019-01-14 19:34:56 +0100

    Update from Tag Manager to Analytics

--- includes/head.php
+++ includes/head.php
@@ -61,15 +61,15 @@
     </head>
 
     <!-- Tracking Snippets -->
-    <!-- GOOGLE TAG MANAGER -->
-    <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-T9PJ53"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-T9PJ53');</script>
-    <!-- END GOOGLE TAG MANAGER -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-37306001-2"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-37306001-2');
+    </script>
 
     <!-- GITHUB BUTTON -->
     <script async defer src="https://buttons.github.io/buttons.js" crossorigin="anonymous"></script>
